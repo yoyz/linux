e4b55ef34019b49f44822df92959d3032417eef1
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2015-11-26 11:21:25 -0500

    inspectio

--- inspectio/inspectio.c
+++ inspectio/inspectio.c
@@ -5,7 +5,13 @@
  
 typedef int     (*orig_open_f_type)(const char *pathname, int flags);
 typedef int     (*orig_close_f_type)(int fd);
+
+typedef int     (*orig_dup2_f_type)(int oldfd,int newfd);
+typedef int     (*orig_dup_f_type)(int oldfd); 
+
 typedef size_t (*orig_read_f_type)(int fd, void *buf, size_t count);
+typedef size_t (*orig_write_f_type)(int fd, void *buf, size_t count);
+
 //typedef ssize_t (*orig_fread_f_type)(void *ptr, size_t size, size_t nbemb, FILE *stream);
 typedef size_t (*orig_fread_f_type)(void * ptr, size_t size,size_t nmemb , FILE * stream);
 typedef FILE * (*orig_fopen_f_type)(const char *path, const char *mode);
@@ -31,6 +37,20 @@ long   tab_fd_to_readcall1024k[1024];
 long   tab_fd_to_readcall2048k[1024];
 long   tab_fd_to_readcall4096k[1024];
 
+long   tab_fd_to_writecall[1024];
+
+long   tab_fd_to_writecall4k[1024];
+long   tab_fd_to_writecall8k[1024];
+long   tab_fd_to_writecall16k[1024];
+long   tab_fd_to_writecall32k[1024];
+long   tab_fd_to_writecall64k[1024];
+long   tab_fd_to_writecall128k[1024];
+long   tab_fd_to_writecall256k[1024];
+long   tab_fd_to_writecall512k[1024];
+long   tab_fd_to_writecall1024k[1024];
+long   tab_fd_to_writecall2048k[1024];
+long   tab_fd_to_writecall4096k[1024];
+
 
 
 long  tab_fd_to_writecall[1024];
@@ -38,7 +58,43 @@ long  tab_fd_to_writecall[1024];
 
 
 char * inspectio_str="__inspect_io__";
- 
+
+
+void printf_info_stdout(int fd,int retcode)
+{
+  //printf("%s close(%d)=%d (%s)\n",inspectio_str,fd,retcode,tab_fd_to_name[fd]);
+  fprintf(stderr,"%s close(%d)=%d rcall=%ld wcall=%ld %s\n",inspectio_str,fd,retcode,tab_fd_to_readcall[fd],tab_fd_to_writecall[fd],tab_fd_to_name[fd]);
+  fprintf(stderr,"%s close readreport  4k=%ld 8k=%ld 16k=%ld 32k=%ld 64k=%ld 128k=%ld 256k=%ld 512k=%ld 1024k=%ld 2048k=%ld 4096k=%ld %s\n",inspectio_str,
+	  tab_fd_to_readcall4k[fd],
+	  tab_fd_to_readcall8k[fd],
+	  tab_fd_to_readcall16k[fd],
+	  tab_fd_to_readcall32k[fd],
+	  tab_fd_to_readcall64k[fd],
+	  tab_fd_to_readcall128k[fd],
+	  tab_fd_to_readcall256k[fd],
+	  tab_fd_to_readcall512k[fd],
+	  tab_fd_to_readcall1024k[fd],
+	  tab_fd_to_readcall2048k[fd],
+	  tab_fd_to_readcall4096k[fd],
+	  tab_fd_to_name[fd]
+	  );  
+  fprintf(stderr,"%s close writereport 4k=%ld 8k=%ld 16k=%ld 32k=%ld 64k=%ld 128k=%ld 256k=%ld 512k=%ld 1024k=%ld 2048k=%ld 4096k=%ld %s\n",inspectio_str,
+	  tab_fd_to_writecall4k[fd],
+	  tab_fd_to_writecall8k[fd],
+	  tab_fd_to_writecall16k[fd],
+	  tab_fd_to_writecall32k[fd],
+	  tab_fd_to_writecall64k[fd],
+	  tab_fd_to_writecall128k[fd],
+	  tab_fd_to_writecall256k[fd],
+	  tab_fd_to_writecall512k[fd],
+	  tab_fd_to_writecall1024k[fd],
+	  tab_fd_to_writecall2048k[fd],
+	  tab_fd_to_writecall4096k[fd],
+	  tab_fd_to_name[fd]
+	  );  
+}
+
+
 
 
 void count_read(int count,int fd)
@@ -56,20 +112,57 @@ void count_read(int count,int fd)
   if (count >  1<<21 && count <= 1<<22)   tab_fd_to_readcall4096k[fd]++;
 }
 
+
+void count_write(int count,int fd)
+{
+  if (count >  0     && count <= 1<<12)   tab_fd_to_writecall4k[fd]++;
+  if (count >  1<<12 && count <= 1<<13)   tab_fd_to_writecall8k[fd]++;
+  if (count >  1<<13 && count <= 1<<14)   tab_fd_to_writecall16k[fd]++;
+  if (count >  1<<14 && count <= 1<<15)   tab_fd_to_writecall32k[fd]++;
+  if (count >  1<<15 && count <= 1<<16)   tab_fd_to_writecall64k[fd]++;
+  if (count >  1<<16 && count <= 1<<17)   tab_fd_to_writecall128k[fd]++;
+  if (count >  1<<17 && count <= 1<<18)   tab_fd_to_writecall256k[fd]++;
+  if (count >  1<<18 && count <= 1<<19)   tab_fd_to_writecall512k[fd]++;
+  if (count >  1<<19 && count <= 1<<20)   tab_fd_to_writecall1024k[fd]++;
+  if (count >  1<<20 && count <= 1<<21)   tab_fd_to_writecall2048k[fd]++;
+  if (count >  1<<21 && count <= 1<<22)   tab_fd_to_writecall4096k[fd]++;
+}
+
 size_t read(int fd, void *buf, size_t count)
 {
   int size;
+  int size_t_count=count;
   orig_read_f_type orig_read;
   orig_read  = (void*)dlsym(RTLD_NEXT,"read");
   size=orig_read(fd,buf,count);
   if (size>=0)
     {
-      fprintf(stderr,"%s read(%d,,%d)=%d\n",inspectio_str,fd,count,size);      
+      fprintf(stderr,"%s read(%d,,%d)=%d\n",inspectio_str,fd,size_t_count,size);      
       tab_fd_to_readcall[fd]++;
       count_read(count,fd);
     }
   return size;
 }
+
+
+size_t write(int fd, void *buf, size_t count)
+{
+  int size;
+  int size_t_count=count;
+  orig_write_f_type orig_write;
+  orig_write  = (void*)dlsym(RTLD_NEXT,"write");
+  size=orig_write(fd,buf,count);
+  if (size>=0)
+    {
+      fprintf(stderr,"%s write(%d,,%d)=%d\n",inspectio_str,fd,size_t_count,size);      
+      tab_fd_to_writecall[fd]++;
+      count_write(count,fd);
+    }
+  return size;
+}
+
+
+
 
 
 size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
@@ -82,7 +175,7 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
   iosize=orig_fread(ptr,size,nmemb,stream);
   if (iosize>=0)
     {
-      fprintf(stderr,"%s fread(%d %d)=%d\n",inspectio_str,fd,count,iosize);      
+      //fprintf(stderr,"%s fread(%d %d)=%d\n",inspectio_str,fd,count,iosize);      
       tab_fd_to_readcall[fd]++;
       count_read(count,fd);
     }
@@ -113,14 +206,15 @@ FILE *fopen(const char *path, const char *mode)
   orig_fopen_f_type orig_fopen;
   orig_fopen  = (void*)dlsym(RTLD_NEXT,"fopen");
   FP=orig_fopen(path,mode);
-  fd=fileno(FP);
-  if (fd>=0)
-    {
-      fprintf(stderr,"%s fopen(%s,%s)=%d\n",inspectio_str,path,mode,fd);
+
+  if (FP!=0)
+  {
+      fd=fileno(FP);
+      //fprintf(stderr,"%s fopen(%s,%s)=%d\n",inspectio_str,path,mode,fd);
       strcpy(tab_fd_to_name[fd],path);
     }
 
-  return FP;
+  return(FP);
 
 }
 
@@ -135,24 +229,32 @@ int close(int fd)
   retcode=orig_close(fd);
   if (retcode>=0)
     {
-      //printf("%s close(%d)=%d (%s)\n",inspectio_str,fd,retcode,tab_fd_to_name[fd]);
-      fprintf(stderr,"%s close(%d)=%d rcall=%d (%s)\n",inspectio_str,fd,retcode,tab_fd_to_readcall[fd],tab_fd_to_name[fd]);
-      fprintf(stderr,"%s close readreport %s 4k=%d 8k=%d 16k=%d 32k=%d 64k=%d 128k=%d 256k=%d 512k=%d 1024k=%d 2048k=%d 4096k=%d\n",inspectio_str,tab_fd_to_name[fd],
-	     tab_fd_to_readcall4k[fd],
-	     tab_fd_to_readcall8k[fd],
-	     tab_fd_to_readcall16k[fd],
-	     tab_fd_to_readcall32k[fd],
-	     tab_fd_to_readcall64k[fd],
-	     tab_fd_to_readcall128k[fd],
-	     tab_fd_to_readcall256k[fd],
-	     tab_fd_to_readcall512k[fd],
-	     tab_fd_to_readcall1024k[fd],
-	     tab_fd_to_readcall2048k[fd],
-	     tab_fd_to_readcall4096k[fd]
-				  );
-
+      printf_info_stdout(fd,retcode);
     }
   return retcode;
 }
+
+int dup2(int oldfd, int newfd)
+{
+  int retcode;
+
+  orig_dup2_f_type orig_dup2;
+  orig_dup2 = (void*)dlsym(RTLD_NEXT,"dup2");
+  retcode=orig_dup2(oldfd,newfd);
+  strcpy(tab_fd_to_name[newfd],tab_fd_to_name[oldfd]);
+  return retcode;
+}
+
+int dup(int oldfd)
+{
+  int retcode;
+
+  orig_dup_f_type orig_dup;
+  orig_dup = (void*)dlsym(RTLD_NEXT,"dup");
+  retcode=orig_dup(oldfd);
+  strcpy(tab_fd_to_name[retcode],tab_fd_to_name[oldfd]);
+  return retcode;
+}
+
 
 
