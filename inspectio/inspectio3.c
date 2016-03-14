186f66a854551f61cdc3d627e51d25df0296265a
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2016-03-14 09:28:59 -0400

    update

--- inspectio/inspectio3.c
+++ inspectio/inspectio3.c
@@ -20,6 +20,9 @@ FILE * FP_STDIN;
 FILE * FP_STDOUT;
 FILE * FP_STDERR;
 
+FILE * FD;
+
+int donothing=0;
 
 typedef void     (*orig_init_f_type)();
 typedef void     (*orig_fini_f_type)();
@@ -50,6 +53,9 @@ enum ioByFileState {
   IOBYFILE_WRITE,
   IOBYFILE_CLOSE,
   IOBYFILE_SEEK,
+  IOBYFILE_DUP,
+  IOBYFILE_DUP2,
+  IOBYFILE_DUP3,
 };
   
 
@@ -201,6 +207,9 @@ void printListIoByFileChainlist(struct ioByFileChainlist * self,FILE * FD)
    char * state_write = "write";
    char * state_close = "close";
    char * state_seek =  "seek";
+   char * state_dup  =  "dup";
+   char * state_dup2 =  "dup2";
+   char * state_dup3 =  "dup3";
    char state[8];
    
    fprintf(FD,"printing\n"); 
@@ -212,8 +221,11 @@ void printListIoByFileChainlist(struct ioByFileChainlist * self,FILE * FD)
        case IOBYFILE_NEW   :	 strcpy(state,state_new); 	 break;
        case IOBYFILE_OPEN  :	 strcpy(state,state_open); 	 break;
        case IOBYFILE_CLOSE :	 strcpy(state,state_close); 	 break;
+       case IOBYFILE_DUP   :	 strcpy(state,state_dup); 	 break;
+       case IOBYFILE_DUP2  :	 strcpy(state,state_dup2); 	 break;
+       case IOBYFILE_DUP3  :	 strcpy(state,state_dup3); 	 break;
        }
-     fprintf(FD,"fd:%5.1d state:%6.6s wtime :%d.%d rtime :%d.%d filename:%147.147s\n",
+     fprintf(FD,"fd:%5.1d state:%6.6s wtime :%d.%d rtime :%d.%d filename:%115.115s\n",
 	     iter->iobfo->fd,
 	     state,
 	     iter->iobfo->tab_fd_to_writetime_seconds,
@@ -223,7 +235,7 @@ void printListIoByFileChainlist(struct ioByFileChainlist * self,FILE * FD)
 	     iter->iobfo->tab_fd_to_name);
 
      
-     fprintf(FD,"writereport 4k=%8.1ld 8k=%8.1ld 16k=%8.1ld 32k=%8.1ld 64k=%8.1ld 128k=%8.1ld 256k=%8.1ld 512k=%8.1ld 1024k=%8.1ld 2048k=%8.1ld 4096k=%8.1ld %16.16s\n",
+     fprintf(FD,"writereport 4k=%8.1ld 8k=%8.1ld 16k=%8.1ld 32k=%8.1ld 64k=%8.1ld 128k=%8.1ld 256k=%8.1ld 512k=%8.1ld 1024k=%8.1ld 2048k=%8.1ld 4096k=%8.1ld \n",
 	     iter->iobfo->tab_fd_to_writecall4k,
 	     iter->iobfo->tab_fd_to_writecall8k,
 	     iter->iobfo->tab_fd_to_writecall16k,
@@ -234,10 +246,9 @@ void printListIoByFileChainlist(struct ioByFileChainlist * self,FILE * FD)
 	     iter->iobfo->tab_fd_to_writecall512k,
 	     iter->iobfo->tab_fd_to_writecall1024k,
 	     iter->iobfo->tab_fd_to_writecall2048k,
-	     iter->iobfo->tab_fd_to_writecall4096k,
-	     iter->iobfo->tab_fd_to_name
+	     iter->iobfo->tab_fd_to_writecall4096k
 	     );
-     fprintf(FD," readreport 4k=%8.1ld 8k=%8.1ld 16k=%8.1ld 32k=%8.1ld 64k=%8.1ld 128k=%8.1ld 256k=%8.1ld 512k=%8.1ld 1024k=%8.1ld 2048k=%8.1ld 4096k=%8.1ld %16.16s\n",
+     fprintf(FD," readreport 4k=%8.1ld 8k=%8.1ld 16k=%8.1ld 32k=%8.1ld 64k=%8.1ld 128k=%8.1ld 256k=%8.1ld 512k=%8.1ld 1024k=%8.1ld 2048k=%8.1ld 4096k=%8.1ld \n",
 	     iter->iobfo->tab_fd_to_readcall4k,
 	     iter->iobfo->tab_fd_to_readcall8k,
 	     iter->iobfo->tab_fd_to_readcall16k,
@@ -248,8 +259,7 @@ void printListIoByFileChainlist(struct ioByFileChainlist * self,FILE * FD)
 	     iter->iobfo->tab_fd_to_readcall512k,
 	     iter->iobfo->tab_fd_to_readcall1024k,
 	     iter->iobfo->tab_fd_to_readcall2048k,
-	     iter->iobfo->tab_fd_to_readcall4096k,
-	     iter->iobfo->tab_fd_to_name
+	     iter->iobfo->tab_fd_to_readcall4096k
 	     );
      
      
@@ -315,8 +325,19 @@ struct ioByFileChainlist * prepareIoByFileChainlist()
 
 
 
-void add_write_time(struct ioByFileChainlist * chainlist_elem,  struct timeval tv0,  struct timeval tv1)
+void add_write_time(int fd,  struct timeval tv0,  struct timeval tv1)
 {
+
+  struct ioByFileChainlist * chainlist_elem;
+  chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
+  if (chainlist_elem==NULL)
+    {
+      chainlist_elem=chainlist_head->new(chainlist_head);
+      chainlist_elem->iobfo->fd=fd;
+      chainlist_elem->iobfo->state=IOBYFILE_OPEN;
+    }
+  
+  
   chainlist_elem->iobfo->tab_fd_to_writetime_seconds=chainlist_elem->iobfo->tab_fd_to_writetime_seconds   + ( tv1.tv_sec  - tv0.tv_sec  );
 
   chainlist_elem->iobfo->tab_fd_to_writetime_useconds=chainlist_elem->iobfo->tab_fd_to_writetime_useconds + ( tv1.tv_usec - tv0.tv_usec );
@@ -333,20 +354,21 @@ void add_write_time(struct ioByFileChainlist * chainlist_elem,  struct timeval t
       chainlist_elem->iobfo->tab_fd_to_writetime_useconds =       chainlist_elem->iobfo->tab_fd_to_writetime_useconds + 1000*1000;
     }
   
-  /*
-  printf("%d.%d      %d.%d         %d.%d       %d.%d\n",
-	 tv0.tv_sec,tv0.tv_usec,
-	 tv1.tv_sec,tv1.tv_usec,
-	 tv1.tv_sec  - tv0.tv_sec,
-	 tv1.tv_usec - tv0.tv_usec,
-	 chainlist_elem->iobfo->tab_fd_to_writetime_seconds,
-	 chainlist_elem->iobfo->tab_fd_to_writetime_useconds);
-  */
 }
 
 
-void add_read_time(struct ioByFileChainlist * chainlist_elem,  struct timeval tv0,  struct timeval tv1)
+void add_read_time(int fd,  struct timeval tv0,  struct timeval tv1)
 {
+
+  struct ioByFileChainlist * chainlist_elem;
+  chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
+  if (chainlist_elem==NULL)
+    {
+      chainlist_elem=chainlist_head->new(chainlist_head);
+      chainlist_elem->iobfo->fd=fd;
+      chainlist_elem->iobfo->state=IOBYFILE_OPEN;
+    }
+  
   chainlist_elem->iobfo->tab_fd_to_readtime_seconds=chainlist_elem->iobfo->tab_fd_to_readtime_seconds   + ( tv1.tv_sec  - tv0.tv_sec  );
 
   chainlist_elem->iobfo->tab_fd_to_readtime_useconds=chainlist_elem->iobfo->tab_fd_to_readtime_useconds + ( tv1.tv_usec - tv0.tv_usec );
@@ -376,6 +398,59 @@ void count_write(int count,int fd)
 {
 }
 
+
+
+void add_write_count(int fd,int count)
+{
+  struct ioByFileChainlist * chainlist_elem;
+  chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
+  if (chainlist_elem==NULL)
+    {
+      chainlist_elem=chainlist_head->new(chainlist_head);
+      chainlist_elem->iobfo->fd=fd;
+      chainlist_elem->iobfo->state=IOBYFILE_WRITE;
+    }
+  
+  if (count >  0     && count <= 1<<12)   chainlist_elem->iobfo->tab_fd_to_writecall4k++;
+  if (count >  1<<12 && count <= 1<<13)   chainlist_elem->iobfo->tab_fd_to_writecall8k++;
+  if (count >  1<<13 && count <= 1<<14)   chainlist_elem->iobfo->tab_fd_to_writecall16k++;
+  if (count >  1<<14 && count <= 1<<15)   chainlist_elem->iobfo->tab_fd_to_writecall32k++;
+  if (count >  1<<15 && count <= 1<<16)   chainlist_elem->iobfo->tab_fd_to_writecall64k++;
+  if (count >  1<<16 && count <= 1<<17)   chainlist_elem->iobfo->tab_fd_to_writecall128k++;
+  if (count >  1<<17 && count <= 1<<18)   chainlist_elem->iobfo->tab_fd_to_writecall256k++;
+  if (count >  1<<18 && count <= 1<<19)   chainlist_elem->iobfo->tab_fd_to_writecall512k++;
+  if (count >  1<<19 && count <= 1<<20)   chainlist_elem->iobfo->tab_fd_to_writecall1024k++;
+  if (count >  1<<20 && count <= 1<<21)   chainlist_elem->iobfo->tab_fd_to_writecall2048k++;
+  if (count >  1<<21 && count <= 1<<22)   chainlist_elem->iobfo->tab_fd_to_writecall4096k++;
+  
+}
+
+void add_read_count(int fd,int count)
+{
+  struct ioByFileChainlist * chainlist_elem;
+  chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
+  if (chainlist_elem==NULL)
+    {
+      chainlist_elem=chainlist_head->new(chainlist_head);
+      chainlist_elem->iobfo->fd=fd;
+      chainlist_elem->iobfo->state=IOBYFILE_READ;
+    }
+  
+  if (count >  0     && count <= 1<<12)   chainlist_elem->iobfo->tab_fd_to_readcall4k++;
+  if (count >  1<<12 && count <= 1<<13)   chainlist_elem->iobfo->tab_fd_to_readcall8k++;
+  if (count >  1<<13 && count <= 1<<14)   chainlist_elem->iobfo->tab_fd_to_readcall16k++;
+  if (count >  1<<14 && count <= 1<<15)   chainlist_elem->iobfo->tab_fd_to_readcall32k++;
+  if (count >  1<<15 && count <= 1<<16)   chainlist_elem->iobfo->tab_fd_to_readcall64k++;
+  if (count >  1<<16 && count <= 1<<17)   chainlist_elem->iobfo->tab_fd_to_readcall128k++;
+  if (count >  1<<17 && count <= 1<<18)   chainlist_elem->iobfo->tab_fd_to_readcall256k++;
+  if (count >  1<<18 && count <= 1<<19)   chainlist_elem->iobfo->tab_fd_to_readcall512k++;
+  if (count >  1<<19 && count <= 1<<20)   chainlist_elem->iobfo->tab_fd_to_readcall1024k++;
+  if (count >  1<<20 && count <= 1<<21)   chainlist_elem->iobfo->tab_fd_to_readcall2048k++;
+  if (count >  1<<21 && count <= 1<<22)   chainlist_elem->iobfo->tab_fd_to_readcall4096k++;
+  
+}
+
+
 size_t read(int fd, void *buf, size_t count)
 {
   int size;
@@ -390,26 +465,16 @@ size_t read(int fd, void *buf, size_t count)
   gettimeofday(&tv0,&tz);
   size=orig_read(fd,buf,count);
   gettimeofday(&tv1,&tz);
+
+  if (donothing)
+    return size;
+  
   if (size>=0)
     {
-      struct ioByFileChainlist * chainlist_elem;
-      chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
-      if (chainlist_elem!=NULL)
-	{
-	  if (count >  0     && count <= 1<<12)   chainlist_elem->iobfo->tab_fd_to_readcall4k++;
-	  if (count >  1<<12 && count <= 1<<13)   chainlist_elem->iobfo->tab_fd_to_readcall8k++;
-	  if (count >  1<<13 && count <= 1<<14)   chainlist_elem->iobfo->tab_fd_to_readcall16k++;
-	  if (count >  1<<14 && count <= 1<<15)   chainlist_elem->iobfo->tab_fd_to_readcall32k++;
-	  if (count >  1<<15 && count <= 1<<16)   chainlist_elem->iobfo->tab_fd_to_readcall64k++;
-	  if (count >  1<<16 && count <= 1<<17)   chainlist_elem->iobfo->tab_fd_to_readcall128k++;
-	  if (count >  1<<17 && count <= 1<<18)   chainlist_elem->iobfo->tab_fd_to_readcall256k++;
-	  if (count >  1<<18 && count <= 1<<19)   chainlist_elem->iobfo->tab_fd_to_readcall512k++;
-	  if (count >  1<<19 && count <= 1<<20)   chainlist_elem->iobfo->tab_fd_to_readcall1024k++;
-	  if (count >  1<<20 && count <= 1<<21)   chainlist_elem->iobfo->tab_fd_to_readcall2048k++;
-	  if (count >  1<<21 && count <= 1<<22)   chainlist_elem->iobfo->tab_fd_to_readcall4096k++;
-	  add_read_time(chainlist_elem,tv0,tv1);
-	}      
-    }
+      add_read_count(fd,size);
+      add_read_time(fd,tv0,tv1);
+    }      
+
   return size;
 }
 
@@ -431,29 +496,16 @@ size_t write(int fd, void *buf, size_t count)
   gettimeofday(&tv0,&tz);
   size=orig_write(fd,buf,count);
   gettimeofday(&tv1,&tz);
-   
-  if (size>=0)
-    {
-      struct ioByFileChainlist * chainlist_elem;
-      chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
-      if (chainlist_elem!=NULL)
-	{
-	    if (count >  0     && count <= 1<<12)   chainlist_elem->iobfo->tab_fd_to_writecall4k++;
-	    if (count >  1<<12 && count <= 1<<13)   chainlist_elem->iobfo->tab_fd_to_writecall8k++;
-	    if (count >  1<<13 && count <= 1<<14)   chainlist_elem->iobfo->tab_fd_to_writecall16k++;
-	    if (count >  1<<14 && count <= 1<<15)   chainlist_elem->iobfo->tab_fd_to_writecall32k++;
-	    if (count >  1<<15 && count <= 1<<16)   chainlist_elem->iobfo->tab_fd_to_writecall64k++;
-	    if (count >  1<<16 && count <= 1<<17)   chainlist_elem->iobfo->tab_fd_to_writecall128k++;
-	    if (count >  1<<17 && count <= 1<<18)   chainlist_elem->iobfo->tab_fd_to_writecall256k++;
-	    if (count >  1<<18 && count <= 1<<19)   chainlist_elem->iobfo->tab_fd_to_writecall512k++;
-	    if (count >  1<<19 && count <= 1<<20)   chainlist_elem->iobfo->tab_fd_to_writecall1024k++;
-	    if (count >  1<<20 && count <= 1<<21)   chainlist_elem->iobfo->tab_fd_to_writecall2048k++;
-	    if (count >  1<<21 && count <= 1<<22)   chainlist_elem->iobfo->tab_fd_to_writecall4096k++;
-	    add_write_time(chainlist_elem,tv0,tv1);
-	}
-    }
+
+  if (donothing)
+    return size;
 
   
+  if (size>=0)
+    {
+      add_write_count(fd,size);
+      add_write_time(fd,tv0,tv1);
+    }
   return size;
 }
 
@@ -469,11 +521,14 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
   orig_fread_f_type orig_fread;
   orig_fread  = (void*)dlsym(RTLD_NEXT,"fread");
   iosize=orig_fread(ptr,size,nmemb,stream);
+
+  if (donothing)
+    return iosize;
+
+  
   if (iosize>=0)
     {
-      //fprintf(stderr,"%s fread(%d %d)=%d\n",inspectio_str,fd,count,iosize);      
-      //tab_fd_to_readcall[fd]++;
-      //count_read(count,fd);
+      //add_read_time
     }
   return iosize;
 }
@@ -501,7 +556,9 @@ int    open(const char *pathname, int flags,...)
        retcode=orig_open(pathname,flags);      
      }
   
-  
+   if (donothing)
+     return retcode;
+
 
   if (retcode>=0)
     {
@@ -523,6 +580,10 @@ FILE *fopen(const char *pathname, const char *mode)
   orig_fopen  = (void*)dlsym(RTLD_NEXT,"fopen");
   FP=orig_fopen(pathname,mode);
 
+  if (donothing)
+    return FP;
+
+  
   if (FP!=0)
   {
       fd=fileno(FP);
@@ -558,6 +619,9 @@ int close(int fd)
   gettimeofday(&tv0,&tz);
   retcode=orig_close(fd);
   gettimeofday(&tv1,&tz);
+
+  if (donothing)
+    return retcode;
   
   if (retcode>=0)
     {
@@ -568,18 +632,8 @@ int close(int fd)
 	{
 	  chainlist_elem->iobfo->fd=-2; 
 	  chainlist_elem->iobfo->state=IOBYFILE_CLOSE;
+	  add_write_time(fd,tv0,tv1);
 	}
-
-      add_write_time(chainlist_elem,tv0,tv1);
-      /* chainlist_elem->iobfo->tab_fd_to_writetime_seconds=chainlist_elem->iobfo->tab_fd_to_writetime_seconds   + tv1.tv_sec  - tv0.tv_sec; */
-      /* chainlist_elem->iobfo->tab_fd_to_writetime_useconds=chainlist_elem->iobfo->tab_fd_to_writetime_useconds + tv1.tv_usec - tv0.tv_usec; */
-      
-      /* if (chainlist_elem->iobfo->tab_fd_to_writetime_useconds>=1000*1000) */
-      /* 	{ */
-      /* 	  chainlist_elem->iobfo->tab_fd_to_writetime_seconds=chainlist_elem->iobfo->tab_fd_to_writetime_seconds+chainlist_elem->iobfo->tab_fd_to_writetime_useconds/(1000*1000); */
-      /* 	  chainlist_elem->iobfo->tab_fd_to_writetime_useconds=chainlist_elem->iobfo->tab_fd_to_writetime_useconds-(1000*1000); */
-      /* 	} */
-
     }
   return retcode;
 }
@@ -588,32 +642,33 @@ int fclose(FILE * FD)
 {
   int retcode;
   int fd=fileno(FD);
+
+  struct timeval tv0;
+  struct timeval tv1;
+  struct timezone tz;
+  
   orig_fclose_f_type orig_fclose;
   orig_fclose = (void*)dlsym(RTLD_NEXT,"fclose");
+
+  gettimeofday(&tv0,&tz);
   retcode=orig_fclose(FD);
+  gettimeofday(&tv1,&tz);
+
+  if (donothing)
+    return retcode;
+
+  
   if (retcode>=0)
     {
-
       struct ioByFileChainlist * chainlist_elem;
-      //chainlist_elem=chainlist_head->getByFd(chainlist_head,8192);
-      //fprintf(FP_STDOUT,"fclose: %d %d\n",chainlist_head,fd);
-
-
-      // HACK chainlist_head could be NULL for an unknown reason here...
-      if (chainlist_head)
+      chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
+      if (chainlist_elem!=NULL)
 	{
-	  chainlist_elem=chainlist_head->getByFd(chainlist_head,fd);
-	  if (chainlist_elem!=NULL)
-	    {
-	      chainlist_elem->iobfo->fd=-2;
-	      chainlist_elem->iobfo->state=IOBYFILE_CLOSE;
-	    }
+	  chainlist_elem->iobfo->fd=-2; 
+	  chainlist_elem->iobfo->state=IOBYFILE_CLOSE;
+	  add_write_time(fd,tv0,tv1);
 	}
 
-
-
-      //strcpy(chainlist_elem->iobfo->tab_fd_to_name,pathname);
-      //printf_info_stdout(fd,retcode);
     }
   return retcode;
 }
@@ -635,60 +690,125 @@ int dup3(int oldfd, int newfd, int flags)
 int dup2(int oldfd, int newfd)
 {
   int retcode;
+  struct ioByFileChainlist * chainlist_elem_orig;
+  struct ioByFileChainlist * chainlist_elem_dest;
 
   orig_dup2_f_type orig_dup2;
   orig_dup2 = (void*)dlsym(RTLD_NEXT,"dup2");
   retcode=orig_dup2(oldfd,newfd);
-  //strcpy(tab_fd_to_name[newfd],tab_fd_to_name[oldfd]);
+
+  if (retcode>=0)
+    {
+      // Tracking the newfd AND IF it exist close it
+      // dup2 do it silently for you
+      chainlist_elem_dest=chainlist_head->getByFd(chainlist_head,newfd);
+      if (chainlist_elem_dest!=NULL)
+	{
+	  chainlist_elem_dest->iobfo->state=IOBYFILE_CLOSE;
+	  chainlist_elem_dest->iobfo->fd=-2;
+	}
+      chainlist_elem_dest=NULL;
+      
+      // Tracking the oldfd and create it, if it doesn't exist
+      chainlist_elem_orig=chainlist_head->getByFd(chainlist_head,oldfd);
+      if (chainlist_elem_orig==NULL)
+	{
+	  chainlist_elem_orig=chainlist_head->new(chainlist_head);
+	  chainlist_elem_orig->iobfo->fd=oldfd;
+	  chainlist_elem_orig->iobfo->state=IOBYFILE_OPEN;
+	}
+
+      // 
+      // Create a newfd
+      // and copy the filename from the oldfd to newfd
+      chainlist_elem_dest=chainlist_head->new(chainlist_head);
+      chainlist_elem_dest->iobfo->fd=newfd;
+      chainlist_elem_dest->iobfo->state=IOBYFILE_DUP2;
+      strcpy(chainlist_elem_dest->iobfo->tab_fd_to_name,chainlist_elem_orig->iobfo->tab_fd_to_name);
+    }
+ 
   return retcode;
 }
 
 int dup(int oldfd)
 {
   int retcode;
-
+  struct ioByFileChainlist * chainlist_elem_orig;
+  struct ioByFileChainlist * chainlist_elem_dest;
+  
   orig_dup_f_type orig_dup;
   orig_dup = (void*)dlsym(RTLD_NEXT,"dup");
   retcode=orig_dup(oldfd);
+
+  if (retcode>=0)
+    {
+      chainlist_elem_orig=chainlist_head->getByFd(chainlist_head,oldfd);
+      if (chainlist_elem_orig==NULL)
+	{
+	  chainlist_elem_orig=chainlist_head->new(chainlist_head);
+	  chainlist_elem_orig->iobfo->fd=oldfd;
+	  chainlist_elem_orig->iobfo->state=IOBYFILE_OPEN;
+	}
+      
+      chainlist_elem_dest=chainlist_head->new(chainlist_head);
+      chainlist_elem_dest->iobfo->fd=retcode;
+      chainlist_elem_dest->iobfo->state=IOBYFILE_DUP;
+      strcpy(chainlist_elem_dest->iobfo->tab_fd_to_name,chainlist_elem_orig->iobfo->tab_fd_to_name);
+    }
+  
+  
   //strcpy(tab_fd_to_name[retcode],tab_fd_to_name[oldfd]);
   return retcode;
 }
 
 
 
+static void con() __attribute__((constructor));
 
-void init() 
+void con() 
 {
-  FILE *  FD;
+  //FILE *  FD;
   pid_t   pid;
   char    logfile[1024];
 
-  /* pid=getpid(); */
-  /* sprintf(logfile,"/tmp/tmp/log.%d",pid); */
+  orig_fopen_f_type orig_fopen;
+  orig_fclose_f_type orig_fclose;
 
-  /* FD=fopen(logfile,"w");  */
-  /* fprintf(FD,"Launching init()\n"); */
-  /* fclose(FD);  */
+  orig_fopen   = (void*)dlsym(RTLD_NEXT,"fopen");
+  orig_fclose  = (void*)dlsym(RTLD_NEXT,"fclose");
+
+  //FP=orig_fopen(pathname,mode);
+  
+  pid=getpid();
+  sprintf(logfile,"/tmp/log.%d",pid);
+
+  FD=orig_fopen(logfile,"w");  
+  fprintf(FD,"Launching init()\n"); 
+  orig_fclose(FD);  
   
   chainlist_head = prepareIoByFileChainlist();
 } 
 
-void fini()
+static void des() __attribute__((destructor));
+
+void des()
 {
-  FILE * FD;
+  //return;
   pid_t pid;
   char logfile[1024];
+
+  orig_fopen_f_type  orig_fopen;
+  orig_fclose_f_type orig_fclose;
+
+  orig_fopen   = (void*)dlsym(RTLD_NEXT,"fopen");
+  orig_fclose  = (void*)dlsym(RTLD_NEXT,"fclose");
+  
   pid=getpid();
   
-  sprintf(logfile,"/tmp/tmp/log.%d",pid);
+  sprintf(logfile,"/tmp/log.%d",pid);
 
-
-
-  FD=fopen(logfile,"a"); 
+  FD=orig_fopen(logfile,"a"); 
   fprintf(FD,"Launching fini()\n"); 
-  //chainlist_head->printList(chainlist_head,FD); 
-  fclose(FD); 
+  chainlist_head->printList(chainlist_head,FD); 
+  orig_fclose(FD); 
 }  
-
-
-
