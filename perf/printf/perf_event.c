45300d3b95ad395aa28bd3e090a8242b2f38e1a1
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-06-12 10:23:56 +0200

    perf_printf

---
+++ perf/printf/perf_event.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <linux/perf_event.h>
+#include <asm/unistd.h>
+
+static long
+perf_event_open(struct perf_event_attr *hw_event, pid_t pid,
+                int cpu, int group_fd, unsigned long flags)
+{
+    int ret;
+
+    ret = syscall(__NR_perf_event_open, hw_event, pid, cpu,
+                   group_fd, flags);
+    return ret;
+}
+
+int
+main(int argc, char **argv)
+{
+    struct perf_event_attr pe;
+    long long count;
+    int fd;
+
+    memset(&pe, 0, sizeof(struct perf_event_attr));
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.size = sizeof(struct perf_event_attr);
+    pe.config = PERF_COUNT_HW_INSTRUCTIONS;
+    pe.disabled = 1;
+    pe.exclude_kernel = 1;
+    pe.exclude_hv = 1;
+
+    pid_t pid = 0;  // measure the current process/thread
+    int cpu = -1;   // measure on any cpu
+    int group_fd = -1;
+    unsigned long flags = 0;
+
+    fd = perf_event_open(&pe, pid, cpu, group_fd, flags);
+    if (fd == -1) {
+       fprintf(stderr, "Error opening leader %llx\n", pe.config);
+       exit(EXIT_FAILURE);
+    }
+
+    ioctl(fd, PERF_EVENT_IOC_RESET, 0);
+    ioctl(fd, PERF_EVENT_IOC_ENABLE, 0);
+
+    printf("Measuring instruction count for this printf\n");
+
+    ioctl(fd, PERF_EVENT_IOC_DISABLE, 0);
+    read(fd, &count, sizeof(long long));
+
+    printf("Used %lld instructions\n", count);
+
+    close(fd);
+}
+ 
