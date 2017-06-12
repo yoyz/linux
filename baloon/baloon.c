659bc84f53e43eaee9886e179a795c5a9903acf9
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-06-12 19:05:02 +0200

    baloon

--- baloon/baloon.c
+++ baloon/baloon.c
@@ -2,55 +2,59 @@
 #include <stdint.h>
 #include <unistd.h> //fork
 #include <stdlib.h> //exit
+#include <signal.h> //signal
 
+#define STR_OMP_NUM_THREADS "16"
 int32_t * buffer;
 float quantity;
 float time;
 
+/*
+
 void daemonize( void )
-{
+{   
+    int64_t i;
     pid_t pid, sid;
+    chdir("/tmp");
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+    umask(0);
+    for (i = 1024; i>=0; i--) { close (i); }
+    sid = setsid();
+
     pid = fork();
     if( pid < 0)
         exit(1);
     else if( pid > 0)
         exit(0);
-    umask(0);
-    sid = setsid();
-    if( sid < 0 )
-    {
-        perror( "daemonize::sid" );
+   pid = fork();
+   if( pid < 0)
         exit(1);
-    }
-    if( chdir("/") < 0 )
-    {
-        perror( "daemonize::chdir" );
-        exit( 1);
-    }
- 
-    close( STDIN_FILENO );
-    close( STDOUT_FILENO );
-    close( STDERR_FILENO );
-}
+    else if( pid > 0)
+        exit(0);
 
+}
+*/
 int main(int argc, char **argv)
 {
         int64_t i=0;
-	int64_t sizeByte;
-	int64_t sizeFourByte;
-	int64_t sizeMiByte;
+        int64_t sizeByte;
+        int64_t sizeFourByte;
+        int64_t sizeMiByte;
 
-	sizeByte=atoi(argv[1]);
-	sizeFourByte=sizeByte/4;
-	sizeMiByte=sizeByte/1024/1024;
+        sizeByte=atoll(argv[1]);
+        sizeFourByte=sizeByte/4;
+        sizeMiByte=sizeByte/1024/1024;
         printf("balloon %lldMiB\n",sizeMiByte);
-	buffer=malloc(sizeof(int32_t)*sizeFourByte);
+        buffer=malloc(sizeof(int32_t)*sizeFourByte);
+        setenv("OMP_NUM_THREADS",STR_OMP_NUM_THREADS,0);
         #pragma omp parallel for
         for (i=0;i<sizeFourByte;i++)
         {
                 buffer[i]=i;
         }
-	daemonize();
-	while(1)
-		sleep(1);
+ //       daemonize();
+        daemon(0,0);
+        while(1)
+                sleep(1);
 }
