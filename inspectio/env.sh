fa78e492fff8a07dfc90c276e1ca1581ea9ff75a
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-07-17 19:09:34 +0200

    env

--- inspectio/env.sh
+++ inspectio/env.sh
@@ -1,7 +1,10 @@
-export    INSPECTIO_PATH=$HOME/lib/inspectio				# path vers la lib
+export    INSPECTIO_PATH=$HOME/lib/inspectio				# 
 export    INSPECTIO_DUMP=$PWD/LOG.${SLURM_JOBID}                        # ou ou dump les log
 mkdir -p $INSPECTIO_DUMP						# on cree le repertoire
 export    LD_PRELOAD=$INSPECTIO_PATH/inspectio.so:$LD_PRELOAD		# on dit a inspectio ou il va dump
 unset     INSPECTIO_ALL                                                	# on desactive le plus gros du log debug
 
-
+grep_inspectio_log()
+{
+	grep ^ LOG.${SLURM_JOBID}/*|grep -v nompi
+}
