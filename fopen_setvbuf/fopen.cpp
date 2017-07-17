f15644ba302360675a6440b9b80f0aa753e7facb
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-07-17 19:09:50 +0200

    fopen iobuff

--- fopen_setvbuf/fopen.cpp
+++ fopen_setvbuf/fopen.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 
-#define MAXBUF 1024*1024*1024              
+//#define MAXBUF 1024*1024*1024*3
 typedef FILE *  (*orig_fopen_f_type)(const char *path, const char *mode);
 char * buffer_setvbuff=NULL;
 
@@ -13,17 +13,18 @@ FILE *fopen(const char *pathname, const char *mode)
 {
   FILE * FP;
   int    fd;
-  size_t size=1024*1024*1024;
+  size_t maxbuf=(size_t)1024*1024*1024*4;
 
   orig_fopen_f_type orig_fopen;
   std::string str(pathname);
   orig_fopen  = (orig_fopen_f_type)dlsym(RTLD_NEXT,"fopen");
   if (str.find("SRME3D_GRID_FILE")!=-1)
   {
-	//printf("catch fopen\n");
+	printf("catch fopen\n");
   	FP=orig_fopen(pathname,mode);
-	buffer_setvbuff=(char*)malloc(sizeof(char)*MAXBUF);
-  	setvbuf(FP,buffer_setvbuff, _IOFBF ,MAXBUF);
+	if (buffer_setvbuff==NULL)
+		buffer_setvbuff=(char*)malloc(sizeof(char)*maxbuf);
+  	setvbuf(FP,buffer_setvbuff, _IOFBF ,maxbuf);
   }
   else
 	FP=orig_fopen(pathname,mode);
