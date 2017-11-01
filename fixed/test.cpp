e30875cc9a1171495117503f15c6614b7f62dbd9
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-11-01 21:03:14 +0100

    fixedpoint from PBSynth

---
+++ fixed/test.cpp
@@ -0,0 +1,43 @@
+#include "fixedpoint.h"
+#include "math.h"
+
+#define SIZE 32
+int main()
+{
+  printf("FixedPoint\n");
+  FixedPoint a1,b1,c1;
+  FixedPoint a2,b2,c2;
+  FixedPoint a3,b3,c3;
+  FixedPoint *tab_fx=(FixedPoint*)malloc(sizeof(FixedPoint)*SIZE);
+  float      *tab_f=(float*)malloc(sizeof(float)*SIZE);
+  a1=FixedPoint(float(1.24));
+  b1=FixedPoint(float(2.33));
+  c1=a1+b1;
+
+  a2=FixedPoint(float(1.24));
+  b2=FixedPoint(float(2.33));
+  c2=a2/b2;
+
+  a3=FixedPoint(float(1.24));
+  b3=FixedPoint(float(2.33));
+  c3=a3-b3;
+
+  for (int i=0;i<SIZE;i++)
+    {
+      tab_fx[i]=float(sin((3.14159)/float(i)));
+      tab_f[i]=sin((3.14159)/float(i));
+    }
+    
+  printf("%f %f %f\n",float(c1),float(c2),float(c3));
+  printf("%d %d %d\n",int(c1),int(c2),int(c3));
+
+  for (int i=2;i<SIZE;i++)
+      printf("%f ",float(tab_fx[i]));
+  printf("\n");
+  
+  for (int i=2;i<SIZE;i++)
+      printf("%f ",float(tab_f[i]));
+  printf("\n");
+	      
+  
+}
