6cdb43c7d207491097fcd4efa1c490ba9978cafd
peyrard.johann@gmail.com <peyrard.johann@gmail.com>
2017-11-30 13:18:23 +0100

    computez.py

---
+++ python/openpyxl/write_to_cell/computez.py
@@ -0,0 +1,17 @@
+#!/home_nfs/peyrardj/anaconda2/bin/python
+#!~/anaconda2/bin/python
+#!/usr/bin/python
+
+from openpyxl import Workbook
+import sys
+import time
+
+book = Workbook()
+sheet = book.active
+
+a=1
+b=2
+c=24
+sheet.cell(row=a,column=b,value=c)
+
+book.save("sample.xlsx")
