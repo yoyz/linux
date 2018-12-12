fdb6056491bf91e546ecf668903021e0bae95092
yoyz <yoyz2k@yahoo.fr>
2018-12-12 14:39:50 +0100

    trace_function.c

--- trace_function_c/trace_function.c
+++ trace_function_c/trace_function.c
@@ -1,4 +1,4 @@
-//gcc  -D_GNU_SOURCE  -finstrument-functions -rdynamic -g  trace_function.c -o trace_function -ldl
+// gcc  -D_GNU_SOURCE  -finstrument-functions -rdynamic -g  trace_function.c -o trace_function -ldl
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,10 +11,10 @@ void __NIF__ __cyg_profile_func_enter (void *this_fn, void *call_site)
   Dl_info info;
   int i;
   int res = dladdr(this_fn, &info);
-  for (i=0;i<__nbcall;i++) printf(" ");
-  printf( "@entering %p %s()\n", this_fn,info.dli_sname );
+  for (i=0;i<__nbcall;i++) printf(">");
+  printf( " entering %p %s()\n", this_fn,info.dli_sname );
   __nbcall++;
-  (call_site);
+  //(call_site);
 }
 
 void __NIF__  __cyg_profile_func_exit (void *this_fn, void *call_site) 
@@ -22,10 +22,10 @@ void __NIF__  __cyg_profile_func_exit (void *this_fn, void *call_site)
   Dl_info info;
   int i;
   int res = dladdr(this_fn, &info);
-  for (i=0;i<__nbcall;i++) printf(" ");
-  printf( "@leaving %p %s()\n", this_fn,info.dli_sname );
+  for (i=0;i<__nbcall;i++) printf("<");
+  printf( " leaving %p %s()\n", this_fn,info.dli_sname );
   __nbcall--;
-  (call_site);
+  //(call_site);
 }
 
 int baz() { printf("hello\n"); }
