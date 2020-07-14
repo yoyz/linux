96638d91956adab6a1fb4a29cba92f3ec08b5c57
yoyz <peyrard.johann@gmail.com>
2020-07-14 16:54:41 +0200

    square.cpp

--- rtaudio/square/square.cpp
+++ rtaudio/square/square.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdint.h>
+#include <unistd.h>
 // Two-channel sawtooth wave generator.
 
 int64_t counter=0;
+int period=100;
 
 int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
          double streamTime, RtAudioStreamStatus status, void *userData )
@@ -17,14 +19,15 @@ int saw( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
 
   for ( i=0; i<nBufferFrames; i++ )
     {
-      if (counter<100)
-	buffer[i]=-16000;
-      if (counter>=100)
-	buffer[i]=-16000;
-      counter++;
-      if (counter>200)
+      if (counter<period)
+	buffer[i]=-8000;
+      if (counter>=period)
+	buffer[i]=8000;
+      if (counter>(period*2))
 	counter=0;
+      counter++;
     }
+  //write(1,buffer,nBufferFrames*2);
   
   return 0;
 }
@@ -37,7 +40,7 @@ int main()
   }
   RtAudio::StreamParameters parameters;
   parameters.deviceId = dac.getDefaultOutputDevice();
-  parameters.nChannels = 2;
+  parameters.nChannels = 1;
   parameters.firstChannel = 0;
   unsigned int sampleRate = 44100;
   unsigned int bufferFrames = 256; // 256 sample frames
